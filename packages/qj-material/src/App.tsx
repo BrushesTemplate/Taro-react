51eaa76dc2f0c521cebf36e568d9dfecc58544d5
devil <devilchinaqin@gmail.com>
2022-10-17 17:06:19 +0800

    fix: react/vue组件

--- packages/qj-material/src/App.tsx
+++ packages/qj-material/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.scss';
 import {Tabs} from 'antd';
 import {AppstoreOutlined, LayoutOutlined} from '@ant-design/icons';
 import TemplateC from "./components/templateC";
 import ModuleC from "./components/moduleC";
-import * as Materials from 's-material-react'
-const App = () => {
+import * as Materials from 's-material-react';
+// @ts-ignore
+import {useLowCodeGraph} from 'qj-shared-library';
 
+const App = () => {
+  const expGraph = useLowCodeGraph(1);
+  useEffect(() => {
+    expGraph.init(Materials);
+  }, [])
   const items = [
     {
       label: (
